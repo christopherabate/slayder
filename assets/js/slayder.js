c0eaf105d7df532dc2abeb8d42f4d8364b70f284
Christopher Abate <contact@christopherabate.com>
2025-02-18 09:29:36 +0100

    Remove tabindexes

--- assets/js/slayder.js
+++ assets/js/slayder.js
@@ -18,53 +18,51 @@
     return translations[document.documentElement.lang || 'en'][key] || key;
   }
 
-  // Current section
+  // Observe section change
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        location.hash = `#${entry.target.id}`;
-
         let main_title = document.querySelector('#cover h1');
         let section_title = entry.target.querySelector('h1') ? null : (function() {
           for (let section = entry.target.previousElementSibling; section; section = section.previousElementSibling) {
             if (section.querySelector('h1')) return section.querySelector('h1');
           }
         })();
-
+        
+        // Change URL
+        if (location.hash !== `#${entry.target.id}`) history.replaceState(null, "", `#${entry.target.id}`);
+        
+        // Change H1
         document.querySelector('h1').innerText = entry.target.id === "cover" ? i18n('training') : (section_title ? section_title.innerText : main_title.innerText);
+        
+        // Change title
         document.title = `${section_title ? `${main_title.innerText} - ${section_title.innerText}` : (entry.target.id === "cover") ? i18n('training') : main_title.innerText} - ${entry.target.querySelector('h1, h2, h3, h4, h5, h6')?.innerText || entry.target.id}`;
-        entry.target.focus();
+
+        // Summary buils
+        document.querySelector('#summary .dropdown-menu').innerHTML = [...document.querySelectorAll('section h1')].map(heading => `
+          <li ${heading === (section_title || entry.target.querySelector('h1')) ? 'class="active"' : ''}><a class="dropdown-item d-flex align-items-center" href="#${heading.closest('section').id}">${heading.textContent}<svg class="ms-auto d-none" width="1em" height="1em" fill="currentColor"><use href="#check"></use></svg></a></li>
+        `).join('');
+
+        // Pagination buid
+        const slides = [...document.querySelectorAll('section')].map(slide => slide.id);
+        const currentIndex = slides.indexOf(entry.target.id);
+        document.querySelector('#pagination .pagination').innerHTML = `
+          <li class="page-item ${currentIndex === 0 ? 'disabled' : ''}">
+            <a ${currentIndex > 0 ? `href="#${slides[currentIndex - 1]}"` : ''} class="page-link" aria-label="${i18n('previous')}"></a>
+          </li>
+          ${slides.map((slide, index) => `
+            <li class="page-item ${currentIndex === index ? 'active' : ''}" ${currentIndex === index ? 'aria-current="page"' : ''}>
+              <a href="#${slide}" class="page-link">${index + 1}</a>
+            </li>
+          `).join('')}
+          <li class="page-item ${currentIndex === slides.length - 1 ? 'disabled' : ''}">
+            <a ${currentIndex < slides.length - 1 ? `href="#${slides[currentIndex + 1]}"` : ''} class="page-link" aria-label="${i18n('next')}"></a>
+          </li>
+        `;
       }
     });
-  }, { threshold: 1 });
+  }, { threshold: .5 });
   document.querySelectorAll('section').forEach(section => observer.observe(section));
-
-  // Summary buils
-  document.querySelector('#summary .dropdown-menu').innerHTML = [...document.querySelectorAll('section h1')].map(heading => `
-    <li><a class="dropdown-item d-flex align-items-center" href="#${heading.closest('section').id}">${heading.textContent}<svg class="ms-auto d-none" width="1em" height="1em" fill="currentColor"><use href="#check"></use></svg></a></li>
-  `).join('');
-
-  // Pagination buid
-  ['load', 'hashchange'].forEach(event => {
-    window.addEventListener(event, () => {
-      const slides = [...document.querySelectorAll('section')].map(slide => slide.id);
-      const currentIndex = slides.indexOf(window.location.hash.split('#').pop());
-
-      document.querySelector('#pagination .pagination').innerHTML = `
-        <li class="page-item ${currentIndex === 0 ? 'disabled' : ''}">
-          <a ${currentIndex > 0 ? `href="#${slides[currentIndex - 1]}"` : ''} class="page-link" aria-label="${i18n('previous')}"></a>
-        </li>
-        ${slides.map((slide, index) => `
-          <li class="page-item ${currentIndex === index ? 'active' : ''}" ${currentIndex === index ? 'aria-current="page"' : ''}>
-            <a href="#${slide}" class="page-link">${index + 1}</a>
-          </li>
-        `).join('')}
-        <li class="page-item ${currentIndex === slides.length - 1 ? 'disabled' : ''}">
-          <a ${currentIndex < slides.length - 1 ? `href="#${slides[currentIndex + 1]}"` : ''} class="page-link" aria-label="${i18n('next')}"></a>
-        </li>
-      `;
-    });
-  });
 
   // Fullscreen enable
   document.querySelector('#fullscreen').addEventListener('click', (event) => {
